0fb5c69d261be477386435d31136884cf496edff
J05H14 <j05h14@yahoo.com>
2017-09-07 16:09:25 -0700

    acm demo

---
+++ acmdemo.py
@@ -0,0 +1,7 @@
+import pets
+
+myPet = pets.pets('jo', 'poodle')
+print(myPet.getname())
+
+myPet.setname("kibble")
+print(myPet.getname())
---
+++ pets.py
@@ -0,0 +1,11 @@
+class pets:
+
+    def __init__(self, name,type):
+        self.name = name
+        self.type = type
+
+    def getname(self):
+        return self.name
+
+    def setname(self, newname):
+        self.name = newname
